6f8fdee9a098941b142b189f09c22eab28654b17
Masashi Aso <maashi.apple.japan@icloud.com>
2019-09-09 07:22:39 +0900

    Initial Commit

---
+++ Booker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Booker
+//
+//  Created by Masashi Aso on 2019/09/09.
+//  Copyright © 2019 Masashi Aso. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
